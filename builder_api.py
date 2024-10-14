319a7eca7a065e87e401ae1a8048369d2e9d27cd
reallyShould <reallyShould@pm.me>
2024-10-14 17:03:34 +0700

    prepare

--- builder_api.py
+++ builder_api.py
@@ -1,8 +1,10 @@
 from flask import Flask, request, jsonify
 import builder
+import configurator.configurator as conf
 
 
 app = Flask(__name__)
+
 
 @app.route("/build", methods=['POST'])
 def build():
@@ -17,6 +19,11 @@ def build():
         return jsonify(bld.get_json())
     else:
         return jsonify({'error': 'No message provided'}), 400
+
+@app.route("/all_mb", methods=['GET']) # TESTING
+def all_mb():
+    c = conf.Configurator()
+    return c.getNamesMB()
 
 if __name__ == '__main__':
     app.run(debug=True)
--- configurator/configurator.py
+++ configurator/configurator.py
@@ -1,12 +1,20 @@
-import constants as cons
+from . import constants as cons
 import pandas as pd
 from flask import jsonify
 
-# print(cons.mb_data["link"][0])
-
 class Configurator:
-    def __init__(self):
-        pass
+    def __init__(self, mbid=-1, cpuid=-1, gpuid=-1, ramid=-1, romid=-1, psuid=-1): # NO NO NO! use list?
+        self.mbid = mbid
+        self.cpuid = cpuid
+        self.gpuid = gpuid
+        self.ramid = ramid
+        self.romid = romid
+        self.psuid = psuid
+
+        unnamed = { # req
+            "socket":"None",
+            "ram_type":"None"
+        }
 
     def getNamesMB(self):
         out_json = {}
@@ -14,3 +22,4 @@ class Configurator:
         for i in range(mb_data_len):
             out_json[i] = cons.mb_data["name"][i]
         return jsonify(out_json)
+    
